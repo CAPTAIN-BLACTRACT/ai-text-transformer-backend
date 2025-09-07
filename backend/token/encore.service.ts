3ab70a4d8936ec212131c4bb5a0338de6181ada9
Leap <git@leap.new>
2025-09-07 18:07:58 +0000

    Manual code change

--- backend/token/encore.service.ts
+++ backend/token/encore.service.ts
@@ -1,17 +1,3 @@
 import { Service } from "encore.dev/service";
 
-export default new Service("token", {
-  cors: {
-    allow_origins: [
-      "chrome-extension://*",
-      "moz-extension://*",
-      "https://staging-ai-text-transformer-backend-b8ri.frontend.encr.app",
-      "http://localhost:*"
-    ],
-    allow_methods: ["POST", "GET", "OPTIONS", "PUT", "DELETE"],
-    allow_headers: ["*"],
-    expose_headers: ["*"],
-    allow_credentials: true,
-    max_age: 86400,
-  },
-});
+export default new Service("transform");
